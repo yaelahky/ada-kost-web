def94344e4f29f88fcef8172a17ca3ed24aebf76
Ananda Rizky Yuliansyah <ananda.yuliansyah@ai.astra.co.id>
2026-03-21 03:19:02 +0700

    chore: bye vercel

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,5 @@
 import type { Metadata } from 'next'
 import { Plus_Jakarta_Sans } from 'next/font/google'
-import { Analytics } from '@vercel/analytics/next'
 import './globals.css'
 
 const plusJakarta = Plus_Jakarta_Sans({ 
@@ -40,7 +39,6 @@ export default function RootLayout({
     <html lang="id">
       <body className={`${plusJakarta.variable} font-sans antialiased`}>
         {children}
-        <Analytics />
       </body>
     </html>
   )
