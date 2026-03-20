1200d888099a40e4db4558ec1fd7c57c4c29754c
Ananda Rizky Yuliansyah <ananda.yuliansyah@ai.astra.co.id>
2026-03-21 03:16:42 +0700

    feat: change lang to id

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
   children: React.ReactNode
 }>) {
   return (
-    <html lang="en">
+    <html lang="id">
       <body className={`${plusJakarta.variable} font-sans antialiased`}>
         {children}
         <Analytics />
